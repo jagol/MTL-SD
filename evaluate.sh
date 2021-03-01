94f19c2e579c475e8a7240e4085b008167cd8d76
Janis Goldzycher <janisgoldzycher@mailbox.org>
2021-03-01 12:05:13 +0100

    rename eval-script, add script to extract predictions

--- evaluate.sh
+++ evaluate.sh
@@ -7,4 +7,5 @@ model_path = "results/${1}/model.tar.gz"
 path_output_file = "results/${1}/predictions.jsonl"
 
 allennlp predict model_path $2 --include-package mtl_sd --predictor multitask --cuda-device $4 --output-file path_output_file
-python3 extract_results.py --path $3
+python3 scripts/extract_results.py --path "results/${3}/predictions.jsonl"
+python3 scripts/evaluate.py --predictions "results/${3}/predictions_extracted.csv" --evaluation "results/${3}/evaluation.json"
