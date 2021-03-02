aa157b214f3dca7490736809f541ff50c37b736e
Janis Goldzycher <janisgoldzycher@mailbox.org>
2021-03-02 10:57:09 +0100

    finish evaluation bash script

--- evaluate.sh
+++ evaluate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # $1: model name
 # $2: test data
-# $4: cuda-device
+# $3: cuda-device
+# $4: label-type, "label_orig" or "label_uni" (check that it's same as specified in StanceDetectionReader)
 
-model_path = "results/${1}/model.tar.gz"
-path_output_file = "results/${1}/predictions.jsonl"
-
-allennlp predict model_path $2 --include-package mtl_sd --predictor multitask --cuda-device $4 --output-file path_output_file
-python3 scripts/extract_results.py --path "results/${3}/predictions.jsonl"
-python3 scripts/evaluate.py --predictions "results/${3}/predictions_extracted.csv" --evaluation "results/${3}/evaluation.json"
+model_path="results/${1}/model.tar.gz"
+path_output_file="results/${1}/predictions.jsonl"
+allennlp predict $model_path $2 --include-package mtl_sd --predictor multitask --cuda-device $3 --output-file $path_output_file --silent
+python3 scripts/extract_results.py --path "results/${1}/predictions.jsonl"
+python3 scripts/evaluate.py --predictions "results/${1}/predictions_extracted.csv" --labels $2 --evaluation "results/${1}/evaluation.json" --vocab "results/${1}/vocabulary" --label_type $4
