1936cfffe4804e7f85f63c6ce6a05e072f9c32a5
Janis Goldzycher <janisgoldzycher@mailbox.org>
2021-03-04 18:58:13 +0100

    adjust compute_metrics for multiclass case

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,7 +1,7 @@
 import csv
 import json
 import argparse
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 import numpy as np
 from sklearn.metrics import f1_score, accuracy_score, recall_score, precision_score
@@ -13,15 +13,17 @@ def load_predictions(fpath: str) -> List[int]:
     with open(fpath) as fin:
         reader = csv.reader(fin)
         for row in reader:
-            class_probs =  [float(num) for num in row]
-            class_id = np.argmax(class_probs)
+            class_probs = [float(num) for num in row]
+            class_id = int(np.argmax(class_probs))
             predictions.append(class_id)
     return predictions
 
 
-def load_labels(fpath_labels: str, dir_path_vocab: str, label_type: str) -> List[int]:
+def load_labels(fpath_labels: str, dir_path_vocab: str, label_type: str
+                ) -> Tuple[List[int], Dict[int, str]]:
     labels = []
     voc = Vocabulary.from_files(dir_path_vocab)
+    label_mapping = {}  # {label_int: label_str}
     with open(fpath_labels) as fin:
         for line in fin:
             test_instance = json.loads(line)
@@ -29,16 +31,28 @@ def load_labels(fpath_labels: str, dir_path_vocab: str, label_type: str) -> List
             task = test_instance['task']
             label_int = voc.get_token_index(label_str, namespace=task+'_labels')
             labels.append(label_int)
-    return labels
+            if label_str not in label_mapping:
+                label_mapping[label_int] = label_str
+    return labels, label_mapping
 
 
-def compute_metrics(predictions: List[int], labels: List[int]):
+def compute_metrics(predictions: List[int], labels: List[int], label_mapping: Dict[int, str]):
     labels = labels[:len(predictions)]
+    f1_per_class = f1_score(y_true=labels, y_pred=predictions, average=None)
+    precision_per_class = precision_score(y_true=labels, y_pred=predictions, average=None)
+    recall_per_class = recall_score(y_true=labels, y_pred=predictions, average=None)
+    precision_by_class = {label_mapping[i]: precision_per_class[i] for i in
+                          range(len(precision_per_class))}
+    recall_by_class = {label_mapping[i]: recall_per_class[i] for i in range(len(recall_per_class))}
+    f1_by_class = {label_mapping[i]: f1_per_class[i] for i in range(len(f1_per_class))}
     return {
-        'f1_macro': f1_score(y_true=labels, y_pred=predictions, average='macro'),
         'accuracy': accuracy_score(y_true=labels, y_pred=predictions),
-        'recall': recall_score(y_true=labels, y_pred=predictions),
-        'precision': precision_score(y_true=labels, y_pred=predictions)
+        'f1_macro': f1_score(y_true=labels, y_pred=predictions, average='macro'),
+        'precision_macro': precision_score(y_true=labels, y_pred=predictions, average='macro'),
+        'recall_macro': recall_score(y_true=labels, y_pred=predictions, average='macro'),
+        'f1_by_class': f1_by_class,
+        'precision_by_class': precision_by_class,
+        'recall_by_class': recall_by_class
     }
 
 
@@ -49,8 +63,8 @@ def write_to_file(metrics: Dict[str, float], fpath: str):
 
 def main(cmd_args):
     predictions = load_predictions(cmd_args.predictions)
-    labels = load_labels(cmd_args.labels, args.vocab, args.label_type)
-    metrics = compute_metrics(predictions, labels)
+    labels, label_mapping = load_labels(cmd_args.labels, args.vocab, args.label_type)
+    metrics = compute_metrics(predictions, labels, label_mapping)
     write_to_file(metrics, cmd_args.evaluation)
 
 
