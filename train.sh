a9e6b8907f6ca6c30a34d9a70a383624c2ac0f6d
Janis Goldzycher <janisgoldzycher@mailbox.org>
2021-03-09 12:58:35 +0100

    add cuda-device to cmd-args

--- train.sh
+++ train.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # $1: config-name
+# $2: cuda-device
 
 path_config="configs/${1}.jsonnet"
 path_results="results/${1}"
-allennlp train $path_config --include-package mtl_sd -s $path_results
+allennlp train $path_config --include-package mtl_sd -s $path_results -o "{'trainer.cuda_device': $2}"
