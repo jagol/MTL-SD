cf9819b3994367292a2ae0f272ddaf2a748b2e88
Janis Goldzycher <janisgoldzycher@mailbox.org>
2021-03-11 19:20:03 +0100

    add conf matrix

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -5,7 +5,8 @@ import os
 from typing import List, Dict, Tuple, Union
 
 import numpy as np
-from sklearn.metrics import f1_score, accuracy_score, recall_score, precision_score
+from sklearn.metrics import (f1_score, accuracy_score, recall_score, precision_score,
+                             confusion_matrix)
 from allennlp.data.vocabulary import Vocabulary
 
 
@@ -54,7 +55,8 @@ def load_corpus_labels(fpath_labels: str, dir_path_vocab: str, label_type: str
 def compute_metrics(predictions: Dict[str, List[int]],
                     labels: Dict[str, List[int]],
                     label_mappings: Dict[str, Dict[int, str]]
-                    ) -> Dict[str, Dict[str, Union[float, Dict[str, float]]]]:
+                    ) -> Tuple[Dict[str, Dict[str, Union[float, Dict[str, float]]]],
+                               Dict[str, List[str]], Dict[str, List[List[int]]]]:
     """
     Args:
         predictions: {corpus: list of predicted labels}
@@ -64,28 +66,33 @@ def compute_metrics(predictions: Dict[str, List[int]],
         {corpus: {metric1: value, metric2: {submetric: value}}}
     """
     metrics = {}
+    headers = {}
+    confms = {}
     assert list(predictions.keys()) == list(labels.keys())
     for corpus in predictions.keys():
         corpus_preds = predictions[corpus]
         corpus_labels = labels[corpus]
         corpus_label_mapping = label_mappings[corpus]
-        corpus_metricss = compute_corpus_metrics(
+        corpus_metrics, corpus_header, corpus_confm = compute_corpus_metrics(
             predictions=corpus_preds,
             labels=corpus_labels,
             label_mapping=corpus_label_mapping
         )
-        metrics[corpus] = corpus_metricss
-    return metrics
+        metrics[corpus] = corpus_metrics
+        headers[corpus] = corpus_header
+        confms[corpus] = corpus_confm
+    return metrics, headers, confms
 
 
 def compute_corpus_metrics(predictions: List[int],
                            labels: List[int],
                            label_mapping: Dict[int, str]
-                           ) -> Dict[str, Union[float, Dict[str, float]]]:
+                           ) -> Tuple[Dict[str, Union[float, Dict[str, float]]],
+                                      List[str], List[List[int]]]:
     """
     Args:
-        predictions: list of predicted labels}
-        labels: list of gold-labels}
+        predictions: list of predicted labels
+        labels: list of gold-labels
         label_mapping: {label-int: label-str}}
     Returns:
         {metric1: value, metric2: {submetric: value}}}
@@ -98,15 +105,23 @@ def compute_corpus_metrics(predictions: List[int],
                           range(len(precision_per_class))}
     recall_by_class = {label_mapping[i]: recall_per_class[i] for i in range(len(recall_per_class))}
     f1_by_class = {label_mapping[i]: f1_per_class[i] for i in range(len(f1_per_class))}
-    return {
+    num_true_per_class = {label_mapping[i]: labels.count(i) for i in set(labels)}
+    num_pred_per_class = {label_mapping[i]: predictions.count(i) for i in set(labels)}
+    conf_header = [label_mapping[i] for i in sorted(set(labels))]
+    conf_matrix = confusion_matrix(y_true=labels, y_pred=predictions,
+                                   labels=[i for i in sorted(set(labels))])
+    metrics = {
         'accuracy': accuracy_score(y_true=labels, y_pred=predictions),
         'f1_macro': f1_score(y_true=labels, y_pred=predictions, average='macro'),
         'precision_macro': precision_score(y_true=labels, y_pred=predictions, average='macro'),
         'recall_macro': recall_score(y_true=labels, y_pred=predictions, average='macro'),
         'f1_by_class': f1_by_class,
         'precision_by_class': precision_by_class,
-        'recall_by_class': recall_by_class
+        'recall_by_class': recall_by_class,
+        'true_labels_per_class': num_true_per_class,
+        'predicted_labels_per_class': num_pred_per_class
     }
+    return metrics, conf_header, conf_matrix
 
 
 def write_to_file(metrics: Dict[str, Dict[str, Union[float, Dict[str, float]]]], fpath: str):
@@ -130,6 +145,21 @@ def write_to_file(metrics: Dict[str, Dict[str, Union[float, Dict[str, float]]]],
         json.dump(metrics_rounded, fout, indent=4)
 
 
+def write_confms_to_file(conf_matrices: Dict[str, List[List[int]]],
+                         conf_headers: Dict[str, List[str]],
+                         eval_path: str):
+    dir_path = '/'.join(eval_path.split('/')[:-1])
+    for corpus in conf_matrices:
+        matrix = conf_matrices[corpus]
+        header = conf_headers[corpus]
+        out_path = os.path.join(dir_path, f'{corpus}_confusion_matrix.csv')
+        with open(out_path, 'w') as fout:
+            writer = csv.writer(fout)
+            writer.writerow(['label'] + header)
+            for i, row in enumerate(matrix):
+                writer.writerow([header[i]] + [str(num) for num in row])
+
+
 def main(cmd_args):
     predictions = {}
     if os.path.isdir(cmd_args.predictions):
@@ -148,8 +178,9 @@ def main(cmd_args):
         label_type=args.label_type,
         data_dir=cmd_args.data_dir
     )
-    metrics = compute_metrics(predictions, labels, label_mappings)
+    metrics, conf_headers, conf_matrices = compute_metrics(predictions, labels, label_mappings)
     write_to_file(metrics, cmd_args.evaluation)
+    write_confms_to_file(conf_matrices, conf_headers, cmd_args.evaluation)
 
 
 if __name__ == '__main__':
