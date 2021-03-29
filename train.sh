7310cc026c8b1561dfb59d9a4a30386ba002f643
Janis Goldzycher <janisgoldzycher@mailbox.org>
2021-03-29 14:52:11 +0200

    fix bash syntax error

--- train.sh
+++ train.sh
@@ -12,5 +12,5 @@ then
 else
   echo "Unknown location: ${5}"
 fipath_config="configs/${1}.jsonnet"
-path_results=$results_dir+"results/${1}"
+path_results="${results_dir}results/${1}"
 allennlp train $path_config --include-package mtl_sd -s $path_results -o "{'trainer.cuda_device': $2}"
